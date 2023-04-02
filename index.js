d3aa3fdbf412d51ffbcccc0a8396fee1a109d085
berkantusta12 <108878204+berkantusta12@users.noreply.github.com>
2023-04-02 03:25:46 +0300

    update to socket

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
+import { WebSocketServer } from 'ws';
 import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import cors from "cors";
 import postRoutes from "./routes/posts.js";
-
 import http from "http";
+
 
 const app = express();
 const server = http.createServer(app);
@@ -18,7 +19,32 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
-app.get("/", (req, res) => {
+const webSocketServer = async () => {
+  
+const socketConnect = () => {
+  const wss = new WebSocketServer({ port: 8080 });
+  let receivedMessage = null
+  wss.on('connection', function connection(ws) {
+      console.log("connected");
+    ws.on('error', console.error);
+  
+    ws.on('message', function message(data) {
+      receivedMessage = data
+      console.log( "recieved", data.toString('utf-8'));
+      // ws.send(receivedMessage);
+      wss.clients.forEach(function (client) {
+          client.send(JSON.stringify(receivedMessage));
+       });
+  });
+  
+  });
+ console.log(" websocket Server started on port 8080");
+}
+
+await socketConnect();
+
+
+ app.get("/", (req, res) => {
   res.json({
     author: "Berkant",
     message: "Hello Kartaca",
@@ -42,5 +68,16 @@ mongoose
 
 server.listen(PORT, () => {
   console.log(`Server running on port: ${PORT}`);
-  console.log(`WebSocket server running on port: ${PORT}`);
+ 
 });
+
+}
+
+webSocketServer();
+
+
+
+
+
+
+
